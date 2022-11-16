89b85c356387db3f74803563bc248c7182a9f7fc
Dmitriy Guseinov <Diman777666@yandex.ru>
2022-11-16 15:59:11 +0400

    feat: errors henldling

---
+++ src/containers/app.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import CardList from "../components/cardList";
+import SearchBox from "../components/SearchBox";
+import './app.css';
+import Scroll from "../components/scroll";
+import ErrorBoundry from "../components/errorBoundry";
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  }
+
+  render() {
+    const { robots, searchField } = this.state;
+    const filtredRobots = robots.filter((robot) => {
+      return robot.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase());
+    });
+
+    return !robots.length ?
+      <h1 className="tc">Loading...</h1> :
+      (
+        <div className="tc">
+          <h1 className="f2">Robo Friends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <ErrorBoundry>
+            <CardList robots={filtredRobots} />
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
+  };
+}
+
+export default App;
