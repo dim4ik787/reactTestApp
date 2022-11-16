48441b65e5f5af3f02307bd7ba04f454ed959527
Dmitriy Guseinov <Diman777666@yandex.ru>
2022-11-16 12:56:59 +0400

    feat: serch + view

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './card';
 import reportWebVitals from './reportWebVitals';
-import { robots } from './robots';
+import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <><Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-  <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-  <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/></>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
